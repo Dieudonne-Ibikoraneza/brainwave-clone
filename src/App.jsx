8314365be8c4fc4b20bf770a951d0b13308842fb
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-10-08 13:46:30 +0200

    Finished the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
